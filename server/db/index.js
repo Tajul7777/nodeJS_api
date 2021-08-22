093757a09e606132553c05e522444a170382248e
tajul7777 <md.tajul.islam.parvaz7777@gmail.com>
2021-08-22 18:54:59 +0200

    Add project files.

---
+++ server/db/index.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+    connectionLimit: 10,
+    password: '',
+    user: "root",
+    database: 'api_data',
+    host: "localhost",
+    port: "3306"
+});
+
+let db = {};
+
+//get all user /api
+db.all = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM categories`, (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+//get a single user /api/:id
+db.one = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM categories WHERE id = ?`,[id], (err, results) => {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(results[0]);
+        });
+    });
+};
+
+module.exports = db;
