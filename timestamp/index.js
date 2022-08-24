e898cc89682fbd47e5e4f7d6186c7c947a7eae74
tajul-tuc <tajul.tuc@gmail.com>
2022-08-24 13:14:04 +0200

    javascript utc function extra

--- timestamp/index.js
+++ timestamp/index.js
@@ -1,8 +1,29 @@
 const utcTimestamp = new Date().getTime();
 
-console.log(utcTimestamp); // 👉️ 16422369....
+console.log(utcTimestamp); //  16422369....
 
 const copy = new Date();
 copy.setTime(utcTimestamp);
 
-console.log(utcTimestamp === copy.getTime()); // 👉️ true
+console.log(utcTimestamp === copy.getTime()); //  true
+
+const date = new Date();
+
+//returns UTC Hour of the date
+console.log(date.getUTCHours()); //  7
+
+//  returns UTC Minutes of the date
+console.log(date.getUTCMinutes()); //  10
+
+//  returns UTC Seconds of the date
+console.log(date.getUTCSeconds()); //  57
+
+//  returns UTC year of the date
+console.log(date.getUTCFullYear()); //  2022
+
+//  returns UTC month (0-11)
+//    0 is January, 11 is December
+console.log(date.getUTCMonth()); //  0
+
+//  returns UTC day of the month (1-31)
+console.log(date.getUTCDate()); //  15
